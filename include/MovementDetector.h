4e27af34d5ed7c676bdbfef8f1f0cfe47f888ffc
Carlos Iván <carlosivanhb@hotmail.com>
2025-04-11 13:51:54 -0600

    Clase refinada para su mejor integración con GUI

--- include/MovementDetector.h
+++ include/MovementDetector.h
@@ -6,13 +6,13 @@
 #include <opencv4/opencv2/opencv.hpp>
 #include <QFutureWatcher>
 #include <QtConcurrent/QtConcurrent>
+#include "UIUtils.h"
+#include <span>
 
 /// @brief Clase capaz de detectar movimiento en una serie de Mat
 class MovementDetector : public QObject {
 
     Q_OBJECT
-
-    static constexpr size_t RectangleMinSize{ 5000 };
 
 public:
 
@@ -24,11 +24,31 @@ public:
     /// @param image Imagen a añadir
     void addImage(const cv::Mat& image);
 
+    /// @brief setHistory Cambia el número de frames a tener en cuenta para la detección de movimiento
+    /// @param history Número de frames a tener en cuenta
+    void setHistory(int history);
+
+    /// @brief setThreshold Cambia el umbral de diferencia entre frames para la detección de movimiento
+    /// @param threshold Umbral de diferencia entre frames
+    void setThreshold(double threshold);
+
+    /// @brief setDetectShadows Cambia si se detectan sombras o no
+    /// @param detectShadows Si se detectan sombras o no
+    void setDetectShadows(bool detectShadows);
+
+    /// @brief setRectangleMinSize Cambia el tamaño mínimo de los cuadros contenedores del movimiento
+    /// @param size Tamaño mínimo de los cuadros contenedores del movimiento
+    void setRectangleMinSize(size_t size);
+
+    /// @brief setDetectionThreshold Cambia el umbral de detección de movimiento
+    /// @param threshold Umbral de detección de movimiento
+    void setDetectionThreshold(int threshold);
+
 signals:
 
     /// @brief movementDetected Emitido cada vez que se detecta movimiento
     /// @param boundingRect Cuadros contenedores del movimiento
-    void movementDetected(std::vector<cv::Rect> boundingRects);
+    void movementDetected(const std::vector<cv::Rect>& boundingRects);
 
 private slots:
 
@@ -36,12 +56,25 @@ private slots:
 
 private:
 
-    QFutureWatcher<std::vector<cv::Rect>>* movementDetectorWorker_m;
+    static constexpr int MaxPixelValue{ 255 };
+
+    QFutureWatcher<std::vector<cv::Rect>> movementDetectorWorker_m;
 
     cv::Ptr<cv::BackgroundSubtractor> backgroundSubstractor_m;
 
-    void startWorker(cv::Mat image);
+    int history_m{ 500 };
+    double threshold_m{ 16 };
+    bool detectShadows_m{ false };
+    bool changesToApplyToBackgroundSubstractor_m{ false };
 
+    size_t detectionRectangleMinimunSize_m{ 5000 };
+    int detectionThreshold_m{ 50 };
+
+    void startWorker(const cv::Mat& image);
+
+    void applyChangesIfRequired();
+
+    std::vector<cv::Rect> processImage(cv::Mat workingImage);
 };
 
 #endif // MOVEMENTDETECTOR_H
--- src/MovementDetector.cpp
+++ src/MovementDetector.cpp
@@ -2,50 +2,110 @@
 
 MovementDetector::MovementDetector(QObject *parent)
     : QObject{parent}
-    , movementDetectorWorker_m{ new QFutureWatcher<std::vector<cv::Rect>>{this} }
-    , backgroundSubstractor_m{ cv::createBackgroundSubtractorMOG2(500, 16, false) }
+    , backgroundSubstractor_m{ cv::createBackgroundSubtractorMOG2(history_m, threshold_m, detectShadows_m) }
 {
-    connect(movementDetectorWorker_m, &QFutureWatcher<std::vector<cv::Rect>>::finished, this, &MovementDetector::emitIfMovementDetected);
+    connect(&movementDetectorWorker_m, &QFutureWatcher<std::vector<cv::Rect>>::finished, this, &MovementDetector::emitIfMovementDetected);
 }
 
 void MovementDetector::addImage(const cv::Mat &image) {
-    if (movementDetectorWorker_m->isRunning() || image.empty()) {
+    if (movementDetectorWorker_m.isRunning() || image.empty()) {
         return;
     }
+
+    applyChangesIfRequired();
 
     startWorker(image);
 }
 
+void MovementDetector::setHistory(int history) {
+    if (history <= 0) {
+        UiUtils::showError("History must be greater than 0");
+        return;
+    }
+
+    history_m = history;
+    changesToApplyToBackgroundSubstractor_m = true;
+}
+
+void MovementDetector::setThreshold(double threshold) {
+    if (threshold < 0) {
+        UiUtils::showError("Threshold must be greater than or equal to 0");
+        return;
+    }
+
+    threshold_m = threshold;
+    changesToApplyToBackgroundSubstractor_m = true;
+}
+
+void MovementDetector::setDetectShadows(bool detectShadows) {
+    detectShadows_m = detectShadows;
+    changesToApplyToBackgroundSubstractor_m = true;
+}
+
+void MovementDetector::setRectangleMinSize(size_t size) {
+    detectionRectangleMinimunSize_m = size;
+}
+
+void MovementDetector::setDetectionThreshold(int threshold) {
+    if (threshold < 0 || threshold > MaxPixelValue) {
+        UiUtils::showError("Detection threshold must be between 0 and " + QString::number(MaxPixelValue));
+        return;
+    }
+
+    detectionThreshold_m = threshold;
+}
+
+void MovementDetector::applyChangesIfRequired() {
+    if (changesToApplyToBackgroundSubstractor_m) {
+        auto newBgSubtractor = cv::createBackgroundSubtractorMOG2(history_m, threshold_m, detectShadows_m);
+
+        if (newBgSubtractor) {
+            backgroundSubstractor_m = newBgSubtractor;
+            
+        } else {
+            UiUtils::showError("Error creating background subtractor with params: " 
+                               "history: " + QString::number(history_m) +
+                               ", threshold: " + QString::number(threshold_m) +
+                               ", detectShadows: " + QString::number(detectShadows_m));
+        }
+
+        changesToApplyToBackgroundSubstractor_m = false;
+    }
+}
+
+std::vector<cv::Rect> MovementDetector::processImage(cv::Mat workingImage) {
+    backgroundSubstractor_m->apply(workingImage, workingImage);
+
+    cv::erode(workingImage, workingImage, {});
+    cv::dilate(workingImage, workingImage, {});
+    cv::threshold(workingImage, workingImage, detectionThreshold_m, MaxPixelValue, cv::THRESH_BINARY);
+
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(workingImage, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    std::vector<cv::Rect> rectangles;
+    for (const auto& i : contours) {
+        if (cv::contourArea(i) > detectionRectangleMinimunSize_m) {
+            rectangles.emplace_back(cv::boundingRect(i));
+        }
+    }
+
+    return rectangles;
+}
+
 void MovementDetector::emitIfMovementDetected() {
-    const auto results{ movementDetectorWorker_m->result() };
+    const auto results{ movementDetectorWorker_m.result() };
 
     if (!results.empty()) {
         emit movementDetected(results);
     }
 }
 
-void MovementDetector::startWorker(cv::Mat image) {
-    movementDetectorWorker_m->setFuture(QtConcurrent::run(
-        [this](cv::Mat image)
-        {
-            backgroundSubstractor_m->apply(image, image);
-
-            cv::erode(image, image, {});
-            cv::dilate(image, image, {});
-
-            cv::threshold(image, image, 50, 255, cv::THRESH_BINARY);
-
-            std::vector<std::vector<cv::Point>> contours;
-            cv::findContours(image, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-            std::vector<cv::Rect> rectangles;
-            for (const auto& i : contours) {
-                if (cv::contourArea(i) > RectangleMinSize) {
-                    rectangles.emplace_back(cv::boundingRect(i));
-                }
-            }
-
-            return rectangles;
+void MovementDetector::startWorker(const cv::Mat& image) {
+    cv::Mat imageCopy = image.clone();
+    movementDetectorWorker_m.setFuture(QtConcurrent::run(
+        [this, imageCopy]() {
+            return this->processImage(imageCopy);
         }
-        , image));
+    ));
 }
